ddabfd1574094c28b29e2e42180b24f26550941d
Kamran Wali <kamran369mars@gmail.com>
2021-08-21 15:03:02 +0100

    Update TriggerTransform.cs
    Button for quitting the game.

--- TransformGC/Assets/TransformGC/Scripts/TriggerTransform.cs
+++ TransformGC/Assets/TransformGC/Scripts/TriggerTransform.cs
@@ -6,5 +6,9 @@ public class TriggerTransform : MonoBehaviour
     [SerializeField] private bool _isPreloadTransform;
 
     private void Awake() { if (_isPreloadTransform) transform.localPosition = transform.localPosition; } // Triggering transform for preloading it at start up
-    private void Update() { if (Input.GetKeyDown(KeyCode.T)) transform.localPosition = transform.localPosition; } // Pressing T triggers transform
+    private void Update() 
+    { 
+        if (Input.GetKeyDown(KeyCode.T)) transform.localPosition = transform.localPosition; // Pressing T triggers transform
+        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit(); // Qutting the game
+    }
 }
